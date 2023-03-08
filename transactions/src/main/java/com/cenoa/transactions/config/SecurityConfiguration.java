462eaf01605f9a4ad3d999864c3350959e2e6d74
Nurullah Macun <nurullah201@gmail.com>
2023-03-08 20:10:56 +0300

    add security config to transactions service

--- transactions/src/main/java/com/cenoa/transactions/config/SecurityConfiguration.java
+++ transactions/src/main/java/com/cenoa/transactions/config/SecurityConfiguration.java
@@ -3,14 +3,9 @@ package com.cenoa.transactions.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.logout.LogoutHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -19,14 +14,9 @@ public class SecurityConfiguration {
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http
-        .csrf().disable()
-        .authorizeHttpRequests()
-        .anyRequest()
-          .authenticated()
-        .and()
-          .sessionManagement()
-          .sessionCreationPolicy(SessionCreationPolicy.STATELESS) ;
+    http.csrf().disable().authorizeHttpRequests()
+            .requestMatchers("/**").permitAll()
+            .anyRequest().authenticated();
 
     return http.build();
   }
